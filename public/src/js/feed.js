7a8f9167b49ca462f858c82e674eb7c1fbb361fb
mehran <m.saghebi.developer@gmail.com>
2023-11-09 09:03:16 +0330

    changing urls to local host

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,95 +1,109 @@
-var shareImageButton = document.querySelector('#share-image-button');
-var createPostArea = document.querySelector('#create-post');
-var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
-var sharedMomentsArea = document.querySelector('#shared-moments');
-var form = document.querySelector('form');
-var titleInput = document.querySelector('#title');
-var locationInput = document.querySelector('#location');
-var videoPlayer = document.querySelector('#player');
-var canvasElement = document.querySelector('#canvas');
-var captureButton = document.querySelector('#capture-btn');
-var imagePicker = document.querySelector('#image-picker');
-var imagePickerArea = document.querySelector('#pick-image');
+var shareImageButton = document.querySelector("#share-image-button");
+var createPostArea = document.querySelector("#create-post");
+var closeCreatePostModalButton = document.querySelector(
+  "#close-create-post-modal-btn"
+);
+var sharedMomentsArea = document.querySelector("#shared-moments");
+var form = document.querySelector("form");
+var titleInput = document.querySelector("#title");
+var locationInput = document.querySelector("#location");
+var videoPlayer = document.querySelector("#player");
+var canvasElement = document.querySelector("#canvas");
+var captureButton = document.querySelector("#capture-btn");
+var imagePicker = document.querySelector("#image-picker");
+var imagePickerArea = document.querySelector("#pick-image");
 var picture;
-var locationBtn = document.querySelector('#location-btn');
-var locationLoader = document.querySelector('#location-loader');
-var fetchedLocation = {lat: 0, lng: 0};
+var locationBtn = document.querySelector("#location-btn");
+var locationLoader = document.querySelector("#location-loader");
+var fetchedLocation = { lat: 0, lng: 0 };
 
-locationBtn.addEventListener('click', function (event) {
-  if (!('geolocation' in navigator)) {
+locationBtn.addEventListener("click", function (event) {
+  if (!("geolocation" in navigator)) {
     return;
   }
   var sawAlert = false;
 
-  locationBtn.style.display = 'none';
-  locationLoader.style.display = 'block';
+  locationBtn.style.display = "none";
+  locationLoader.style.display = "block";
 
-  navigator.geolocation.getCurrentPosition(function (position) {
-    locationBtn.style.display = 'inline';
-    locationLoader.style.display = 'none';
-    fetchedLocation = {lat: position.coords.latitude, lng: 0};
-    locationInput.value = 'In Munich';
-    document.querySelector('#manual-location').classList.add('is-focused');
-  }, function (err) {
-    console.log(err);
-    locationBtn.style.display = 'inline';
-    locationLoader.style.display = 'none';
-    if (!sawAlert) {
-      alert('Couldn\'t fetch location, please enter manually!');
-      sawAlert = true;
-    }
-    fetchedLocation = {lat: 0, lng: 0};
-  }, {timeout: 7000});
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      locationBtn.style.display = "inline";
+      locationLoader.style.display = "none";
+      fetchedLocation = { lat: position.coords.latitude, lng: 0 };
+      locationInput.value = "In Munich";
+      document.querySelector("#manual-location").classList.add("is-focused");
+    },
+    function (err) {
+      console.log(err);
+      locationBtn.style.display = "inline";
+      locationLoader.style.display = "none";
+      if (!sawAlert) {
+        alert("Couldn't fetch location, please enter manually!");
+        sawAlert = true;
+      }
+      fetchedLocation = { lat: 0, lng: 0 };
+    },
+    { timeout: 7000 }
+  );
 });
 
 function initializeLocation() {
-  if (!('geolocation' in navigator)) {
-    locationBtn.style.display = 'none';
+  if (!("geolocation" in navigator)) {
+    locationBtn.style.display = "none";
   }
 }
 
 function initializeMedia() {
-  if (!('mediaDevices' in navigator)) {
+  if (!("mediaDevices" in navigator)) {
     navigator.mediaDevices = {};
   }
 
-  if (!('getUserMedia' in navigator.mediaDevices)) {
+  if (!("getUserMedia" in navigator.mediaDevices)) {
     navigator.mediaDevices.getUserMedia = function (constraints) {
-      var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+      var getUserMedia =
+        navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
       if (!getUserMedia) {
-        return Promise.reject(new Error('getUserMedia is not implemented!'));
+        return Promise.reject(new Error("getUserMedia is not implemented!"));
       }
 
       return new Promise(function (resolve, reject) {
         getUserMedia.call(navigator, constraints, resolve, reject);
       });
-    }
+    };
   }
 
-  navigator.mediaDevices.getUserMedia({video: true})
+  navigator.mediaDevices
+    .getUserMedia({ video: true })
     .then(function (stream) {
       videoPlayer.srcObject = stream;
-      videoPlayer.style.display = 'block';
+      videoPlayer.style.display = "block";
     })
     .catch(function (err) {
-      imagePickerArea.style.display = 'block';
+      imagePickerArea.style.display = "block";
     });
 }
 
-captureButton.addEventListener('click', function (event) {
-  canvasElement.style.display = 'block';
-  videoPlayer.style.display = 'none';
-  captureButton.style.display = 'none';
-  var context = canvasElement.getContext('2d');
-  context.drawImage(videoPlayer, 0, 0, canvas.width, videoPlayer.videoHeight / (videoPlayer.videoWidth / canvas.width));
+captureButton.addEventListener("click", function (event) {
+  canvasElement.style.display = "block";
+  videoPlayer.style.display = "none";
+  captureButton.style.display = "none";
+  var context = canvasElement.getContext("2d");
+  context.drawImage(
+    videoPlayer,
+    0,
+    0,
+    canvas.width,
+    videoPlayer.videoHeight / (videoPlayer.videoWidth / canvas.width)
+  );
   videoPlayer.srcObject.getVideoTracks().forEach(function (track) {
     track.stop();
   });
   picture = dataURItoBlob(canvasElement.toDataURL());
 });
 
-imagePicker.addEventListener('change', function (event) {
+imagePicker.addEventListener("change", function (event) {
   picture = event.target.files[0];
 });
 
@@ -97,7 +111,7 @@ function openCreatePostModal() {
   // createPostArea.style.display = 'block';
   // setTimeout(function() {
   setTimeout(function () {
-    createPostArea.style.transform = 'translateY(0)';
+    createPostArea.style.transform = "translateY(0)";
   }, 1);
   initializeMedia();
   initializeLocation();
@@ -108,10 +122,10 @@ function openCreatePostModal() {
     deferredPrompt.userChoice.then(function (choiceResult) {
       console.log(choiceResult.outcome);
 
-      if (choiceResult.outcome === 'dismissed') {
-        console.log('User cancelled installation');
+      if (choiceResult.outcome === "dismissed") {
+        console.log("User cancelled installation");
       } else {
-        console.log('User added to home screen');
+        console.log("User added to home screen");
       }
     });
 
@@ -129,36 +143,35 @@ function openCreatePostModal() {
 }
 
 function closeCreatePostModal() {
-  imagePickerArea.style.display = 'none';
-  videoPlayer.style.display = 'none';
-  canvasElement.style.display = 'none';
-  locationBtn.style.display = 'inline';
-  locationLoader.style.display = 'none';
-  captureButton.style.display = 'inline';
+  imagePickerArea.style.display = "none";
+  videoPlayer.style.display = "none";
+  canvasElement.style.display = "none";
+  locationBtn.style.display = "inline";
+  locationLoader.style.display = "none";
+  captureButton.style.display = "inline";
   if (videoPlayer.srcObject) {
     videoPlayer.srcObject.getVideoTracks().forEach(function (track) {
       track.stop();
     });
   }
   setTimeout(function () {
-    createPostArea.style.transform = 'translateY(100vh)';
+    createPostArea.style.transform = "translateY(100vh)";
   }, 1);
   // createPostArea.style.display = 'none';
 }
 
-shareImageButton.addEventListener('click', openCreatePostModal);
+shareImageButton.addEventListener("click", openCreatePostModal);
 
-closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+closeCreatePostModalButton.addEventListener("click", closeCreatePostModal);
 
 // Currently not in use, allows to save assets in cache on demand otherwise
 function onSaveButtonClicked(event) {
-  console.log('clicked');
-  if ('caches' in window) {
-    caches.open('user-requested')
-      .then(function (cache) {
-        cache.add('https://httpbin.org/get');
-        cache.add('/src/images/sf-boat.jpg');
-      });
+  console.log("clicked");
+  if ("caches" in window) {
+    caches.open("user-requested").then(function (cache) {
+      cache.add("https://httpbin.org/get");
+      cache.add("/src/images/sf-boat.jpg");
+    });
   }
 }
 
@@ -169,22 +182,22 @@ function clearCards() {
 }
 
 function createCard(data) {
-  var cardWrapper = document.createElement('div');
-  cardWrapper.className = 'shared-moment-card mdl-card mdl-shadow--2dp';
-  var cardTitle = document.createElement('div');
-  cardTitle.className = 'mdl-card__title';
-  cardTitle.style.backgroundImage = 'url(' + data.image + ')';
-  cardTitle.style.backgroundSize = 'cover';
+  var cardWrapper = document.createElement("div");
+  cardWrapper.className = "shared-moment-card mdl-card mdl-shadow--2dp";
+  var cardTitle = document.createElement("div");
+  cardTitle.className = "mdl-card__title";
+  cardTitle.style.backgroundImage = "url(" + data.image + ")";
+  cardTitle.style.backgroundSize = "cover";
   cardWrapper.appendChild(cardTitle);
-  var cardTitleTextElement = document.createElement('h2');
-  cardTitleTextElement.style.color = 'white';
-  cardTitleTextElement.className = 'mdl-card__title-text';
+  var cardTitleTextElement = document.createElement("h2");
+  cardTitleTextElement.style.color = "white";
+  cardTitleTextElement.className = "mdl-card__title-text";
   cardTitleTextElement.textContent = data.title;
   cardTitle.appendChild(cardTitleTextElement);
-  var cardSupportingText = document.createElement('div');
-  cardSupportingText.className = 'mdl-card__supporting-text';
+  var cardSupportingText = document.createElement("div");
+  cardSupportingText.className = "mdl-card__supporting-text";
   cardSupportingText.textContent = data.location;
-  cardSupportingText.style.textAlign = 'center';
+  cardSupportingText.style.textAlign = "center";
   // var cardSaveButton = document.createElement('button');
   // cardSaveButton.textContent = 'Save';
   // cardSaveButton.addEventListener('click', onSaveButtonClicked);
@@ -201,86 +214,92 @@ function updateUI(data) {
   }
 }
 
-var url = 'https://pwagram-99adf.firebaseio.com/posts.json';
 var networkDataReceived = false;
-
-fetch(url)
+// 'https://pwagram-99adf.firebaseio.com/posts.json';
+fetch("http://localhost:8585/getPosts")
   .then(function (res) {
     return res.json();
   })
   .then(function (data) {
     networkDataReceived = true;
-    console.log('From web', data);
+    console.log("From web", data);
     var dataArray = [];
     for (var key in data) {
       dataArray.push(data[key]);
     }
     updateUI(dataArray);
+  })
+  .catch((err) => {
+    console.log(err);
   });
 
-if ('indexedDB' in window) {
-  readAllData('posts')
-    .then(function (data) {
-      if (!networkDataReceived) {
-        console.log('From cache', data);
-        updateUI(data);
-      }
-    });
+if ("indexedDB" in window) {
+  readAllData("posts").then(function (data) {
+    if (!networkDataReceived) {
+      console.log("From cache", data);
+      updateUI(data);
+    }
+  });
 }
 
 function sendData() {
   var id = new Date().toISOString();
   var postData = new FormData();
-  postData.append('id', id);
-  postData.append('title', titleInput.value);
-  postData.append('location', locationInput.value);
-  postData.append('rawLocationLat', fetchedLocation.lat);
-  postData.append('rawLocationLng', fetchedLocation.lng);
-  postData.append('file', picture, id + '.png');
+  postData.append("id", id);
+  postData.append("title", titleInput.value);
+  postData.append("location", locationInput.value);
+  postData.append("rawLocationLat", fetchedLocation.lat);
+  postData.append("rawLocationLng", fetchedLocation.lng);
+  postData.append("file", picture, id + ".png");
 
-  fetch('https://us-central1-pwagram-99adf.cloudfunctions.net/storePostData', {
-    method: 'POST',
-    body: postData
-  })
+  fetch(
+    "http://localhost:8585/addPost", //  "https://us-central1-pwagram-99adf.cloudfunctions.net/storePostData"
+    {
+      method: "POST",
+      body: postData,
+    }
+  )
     .then(function (res) {
-      console.log('Sent data', res);
+      console.log("Sent data", res);
       updateUI();
     })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
-form.addEventListener('submit', function (event) {
+form.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  if (titleInput.value.trim() === '' || locationInput.value.trim() === '') {
-    alert('Please enter valid data!');
+  if (titleInput.value.trim() === "" || locationInput.value.trim() === "") {
+    alert("Please enter valid data!");
     return;
   }
 
   closeCreatePostModal();
 
-  if ('serviceWorker' in navigator && 'SyncManager' in window) {
-    navigator.serviceWorker.ready
-      .then(function (sw) {
-        var post = {
-          id: new Date().toISOString(),
-          title: titleInput.value,
-          location: locationInput.value,
-          picture: picture,
-          rawLocation: fetchedLocation
-        };
-        writeData('sync-posts', post)
-          .then(function () {
-            return sw.sync.register('sync-new-posts');
-          })
-          .then(function () {
-            var snackbarContainer = document.querySelector('#confirmation-toast');
-            var data = {message: 'Your Post was saved for syncing!'};
-            snackbarContainer.MaterialSnackbar.showSnackbar(data);
-          })
-          .catch(function (err) {
-            console.log(err);
-          });
-      });
+  if ("serviceWorker" in navigator && "SyncManager" in window) {
+    navigator.serviceWorker.ready.then(function (sw) {
+      var post = {
+        id: new Date().toISOString(),
+        title: titleInput.value,
+        location: locationInput.value,
+        picture: picture,
+        rawLocation: fetchedLocation,
+      };
+      writeData("sync-posts", post)
+        .then(function () {
+          return sw.sync.register("sync-new-posts");
+        })
+        .then(function () {
+          var snackbarContainer = document.querySelector("#confirmation-toast");
+          var data = { message: "Your Post was saved for syncing!" };
+          snackbarContainer.MaterialSnackbar.showSnackbar(data);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    });
   } else {
     sendData();
   }
---
+++ sw.strategies.js
@@ -0,0 +1,59 @@
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request)
+          .then(function (res) {
+            return caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
+              cache.put(event.request.url, res.clone());
+              return res;
+            });
+          })
+          .catch(function (err) {
+            return caches.open(CACHE_STATIC_NAME).then(function (cache) {
+              return cache.match("/offline.html");
+            });
+          });
+      }
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    fetch(event.request)
+      .then(function (res) {
+        return caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
+          cache.put(event.request.url, res.clone());
+          return res;
+        });
+      })
+      .catch(function (err) {
+        return caches.match(event.request);
+      })
+  );
+});
+
+// Cache - only;
+self.addEventListener("fetch", function (event) {
+  event.respondWith(caches.match(event.request));
+});
+
+// Network - only;
+self.addEventListener("fetch", function (event) {
+  event.respondWith(fetch(event.request));
+});
+
+/* ----------------------------------------------------------------------------------------- */
+
+function trimCache(cacheName, maxItems) {
+  caches.open(cacheName).then(function (cache) {
+    return cache.keys().then(function (keys) {
+      if (keys.length > maxItems) {
+        cache.delete(keys[0]).then(trimCache(cacheName, maxItems));
+      }
+    });
+  });
+}
